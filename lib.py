d923f86c66b96a35cb180b214dbcc4c7cd102bb9
YourUser <my_email@example.com>
2019-02-10 09:27:59 +0300

    Так тоже не стоит делать

--- lib.py
+++ lib.py
@@ -16,13 +16,7 @@ def is_creditable(age, salary):
     max_age = 60
     min_salary = 30_000
 #Так делать не нужно
-    if age >= min_age:
-        if age <= max_age:
-            if salary >= min_salary:
-                return True
-            else:
-                return False
-        else:
-            return False
+    if age >= min_age and age <= max_age and salary >= min_salary:
+        return True
     else:
         return False
